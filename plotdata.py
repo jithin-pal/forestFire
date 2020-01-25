bdc0db033b963a5b08f8ebae7ae6c1a3e03059ab
jithin kumar <44967770+jithin-pal@users.noreply.github.com>
2020-01-25 22:48:49 +0000

    Add files via upload

--- plotdata.py
+++ plotdata.py
@@ -1,29 +1,61 @@
 import matplotlib.pyplot as plt
-import time
 import pandas as pd
 import numpy as np
 import json
 from matplotlib.animation import FuncAnimation
-from sklearn.linear_model import LogisticRegression
-import csv
+
+data = pd.read_csv('MergeData.csv')
+temp1 = data['temperature1']
+humidity1 = data['humidity1']
+flames1 = data['flame1']
+temp2 = data['temperature2']
+humidity2 = data['humidity2']
+flames2 = data['flames2']
 
 plt.style.use('seaborn')
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
 
-def animate(i):
+ax1.plot(temp1, label='Tem D1', color ='red')
+ax1.plot(humidity1, label='Hum D1', color ='blue')
+ax1.plot(flames1, label='Flames D1', color ='orange')
 
-    data = pd.read_csv('MergeData.csv')
-    temp1 = data['temperature1']
-    humidity1 = data['humidity1']
-    flames1 = data['flame1']
-    temp2 = data['temperature2']
-    humidity2 = data['humidity2']
-    flames2 = data['flames2']
-    plt.cla()
-    plt.plot(temp1, label=' temp', color='red' )
-    plt.plot(humidity1, label=' humidity', color ='blue' )
-    plt.plot(flames1, label='flames', color='orange')
-    plt.legend(loc = 'upper left')
-anim = FuncAnimation(plt.gcf(), animate, interval=800)
+ax2.plot(temp2, label='Tem D2', color ='red')
+ax2.plot(humidity2, label='Hum D2', color ='blue')
+ax2.plot(flames2, label='Flame D2', color ='orange')
+ax1.legend()
+ax2.legend()
 plt.tight_layout()
 plt.show()
+
+
+
+
+# def animate(i):
+
+#     data = pd.read_csv('MergeData.csv')
+#     temp1 = data['temperature1']
+#     humidity1 = data['humidity1']
+#     flames1 = data['flame1']
+#     temp2 = data['temperature2']
+#     humidity2 = data['humidity2']
+#     flames2 = data['flames2']
+#     plt.cla()
+#     plt.plot(temp1, label=' temp', color='red' )
+#     plt.plot(humidity1, label=' humidity', color ='blue' )
+#     plt.plot(flames1, label='flames', color='orange')
+#     plt.legend(loc = 'upper right')
+# anim = FuncAnimation(plt.gcf(), animate, interval=800)
+# plt.tight_layout()
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
 
